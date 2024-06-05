c8ef08dac2ecf560cf3f7e984a778a053c7e8bc4
Renaldi <109418521+RenaldKS@users.noreply.github.com>
2024-06-05 17:36:29 +0700

    Added Basic Login and Register Function
    Just Basic implementasion, not connected to the database for now

---
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/LoginActivity.java
@@ -0,0 +1,54 @@
+package nodomain.freeyourgadget.gadgetbridge.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import nodomain.freeyourgadget.gadgetbridge.R;
+
+public class LoginActivity extends Activity {
+
+    private EditText usernameField;
+    private EditText passwordField;
+    private Button loginButton;
+    private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameField = (EditText) findViewById(R.id.username);
+        passwordField = (EditText) findViewById(R.id.password);
+        loginButton = (Button) findViewById(R.id.login_button);
+        registerButton = (Button) findViewById(R.id.registerButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    // Check if the username and password are correct
+                    if (usernameField.getText().toString().equals("admin") &&
+                            passwordField.getText().toString().equals("admin")) {
+                        // If the username and password are correct, start the MainActivity
+                        startActivity(new Intent(LoginActivity.this, ControlCenterv2.class));
+                    } else {
+                        // If the username and password are incorrect, show a toast
+                        Toast.makeText(LoginActivity.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+}
